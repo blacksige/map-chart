1facbb5aea7a7e2032dd689a4860d262581615cb
wudequan <1438275095@qq.com>
2023-04-14 20:12:08 +0800

    map优化

--- src/components/mapChart/index.vue
+++ src/components/mapChart/index.vue
@@ -8,14 +8,17 @@
 </template>
 
 <script>
-let echarts = require('echarts/lib/echarts');
-import { provinces, cityMap } from './config/index.js';
-// import $ from '../utils/jquery';
-require('echarts/lib/chart/map');
-require('echarts/map/js/china');
-
+let echarts = require("echarts/lib/echarts");
+import { provinces, cityMap } from "./config/index.js";
+// import FileSaver from "file-saver";
+import mapDist from "../../utils/dist";
+require("echarts/lib/chart/map");
+require("echarts/map/js/china");
+// echarts.registerMap('china', geoJson)
+// import * as L from "leaflet";
+import * as turf from "@turf/turf";
 export default {
-  name: 'mapChart',
+  name: "mapChart",
   data() {
     return {
       // 坐标
@@ -34,22 +37,24 @@ export default {
       },
       // 数据
       rawData: [
-        ['北京', 5, 20, 30],
-        ['上海', 10, 10, 30],
-        ['广州', 10, 50, 30],
-        ['杭州', 10, 20, 3],
-        ['重庆', 10, 20, 8],
-        ['青岛', 10, 20, 10],
-        ['厦门', 10, 20, 4],
-        ['福州', 10, 10, 30],
-        ['兰州', 10, 15, 30],
-        ['长沙', 10, 25, 30],
-        ['南京', 10, 20, 5],
+        ["北京", 5, 20, 30],
+        ["上海", 10, 10, 30],
+        ["广州", 10, 50, 30],
+        ["杭州", 10, 20, 3],
+        ["重庆", 10, 20, 8],
+        ["青岛", 10, 20, 10],
+        ["厦门", 10, 20, 4],
+        ["福州", 10, 10, 30],
+        ["兰州", 10, 15, 30],
+        ["长沙", 10, 25, 30],
+        ["南京", 10, 20, 5],
       ],
-      legendData: ['在办工单量', '已办工单量', '其他'],
+      legendData: ["在办工单量", "已办工单量", "其他"],
 
-      mapName: 'china',
+      mapName: "china",
       myChart: {},
+
+      resData: {},
     };
   },
   watch: {
@@ -59,8 +64,20 @@ export default {
   },
   mounted() {
     // 初始化加载
-    this.myChart = echarts.init(document.getElementById('container'));
+    this.myChart = echarts.init(document.getElementById("container"));
     this.mapChart(true);
+    console.log(mapDist);
+    // Object.keys(provinces).forEach((item) => {
+    //   this.getMapInfo(item, provinces[item]);
+    // });
+
+    // setTimeout(() => {
+    //   console.log(this.resData);
+    //   let blob = new Blob([JSON.stringify(this.resData)], {
+    //     type: "text/plain;charset=utf-8",
+    //   });
+    //   FileSaver.saveAs(blob, "a.txt");
+    // }, 8000);
   },
   beforeDestroy() {
     this.myChart = {};
@@ -69,7 +86,7 @@ export default {
     // 返回全国视图
     backMap() {
       this.$nextTick(() => {
-        this.mapName = 'china';
+        this.mapName = "china";
         this.mapChart();
       });
     },
@@ -78,7 +95,7 @@ export default {
     mapChart(first = false) {
       let that = this;
       first &&
-        window.addEventListener('resize', () => {
+        window.addEventListener("resize", () => {
           that.myChart.resize();
         });
 
@@ -86,29 +103,29 @@ export default {
         animation: true,
         // 地图背景颜色
         tooltip: {
-          trigger: 'axis',
+          trigger: "axis",
         },
         geo: {
           map: that.mapName,
           // silent: true,
           roam: false,
           aspectScale: 0.75,
-          zoom: 1, // 地图初始大小
+          zoom: 0.9, // 地图初始大小
           // center: [105.366794, 35.400309], // 初始中心位置
           label: {
             normal: {
               show: true,
-              fontSize: '12',
-              color: '#D6D6F0',
+              fontSize: "12",
+              color: "#D6D6F0",
             },
           },
           // 地区块儿颜色
           itemStyle: {
             normal: {
-              areaColor: '#4c91f7',
+              areaColor: "#4c91f7",
               borderWidth: 1,
-              borderColor: '#1670d3',
-              shadowColor: '#004199',
+              borderColor: "#1670d3",
+              shadowColor: "#004199",
               shadowBlur: 15,
               shadowOffsetX: 3,
               shadowOffsetY: 5,
@@ -121,8 +138,8 @@ export default {
           },
         },
         grid: {
-          left: '5%',
-          top: '5%',
+          left: "5%",
+          top: "5%",
         },
         series: [],
       };
@@ -132,10 +149,10 @@ export default {
           title: [],
           grid: [],
           legend: {
-            x: '12%',
-            y: '85%',
-            orient: 'vertical',
-            icon: 'circle',
+            x: "12%",
+            y: "85%",
+            orient: "vertical",
+            icon: "circle",
             data: that.legendData,
           },
           series: [],
@@ -143,8 +160,8 @@ export default {
 
         echarts.util.each(that.rawData, function(dataItem, idx) {
           let geoCoord = that.geoCoordMap[dataItem[0]];
-          let coord = that.myChart.convertToPixel('geo', geoCoord);
-          idx += '';
+          let coord = that.myChart.convertToPixel("geo", geoCoord);
+          idx += "";
           let inflationData = that.legendData.map((item, index) => {
             return {
               name: item,
@@ -173,7 +190,7 @@ export default {
           });
           option.series.push({
             id: idx,
-            type: 'pie',
+            type: "pie",
             label: {
               normal: {
                 show: false,
@@ -191,7 +208,7 @@ export default {
               },
             },
             radius:
-              total > 50 ? '4%' : total > 40 ? '3%' : total > 30 ? '2%' : '1%',
+              total > 50 ? "4%" : total > 40 ? "3%" : total > 30 ? "2%" : "1%",
             center: coord,
             data: inflationData,
             z: 100,
@@ -199,7 +216,7 @@ export default {
               normal: {
                 color: function(params) {
                   // 柱状图每根柱子颜色
-                  let colorList = ['#FF8A54', '#F3E5A2', '#00EAFF'];
+                  let colorList = ["#FF8A54", "#F3E5A2", "#00EAFF"];
                   return colorList[params.dataIndex];
                 },
               },
@@ -214,7 +231,7 @@ export default {
 
       // 点击触发
       first &&
-        that.myChart.on('click', param => {
+        that.myChart.on("click", (param) => {
           if (param.name in provinces) {
             // 处理省模块
             let names = param.name;
@@ -249,13 +266,13 @@ export default {
             that.mapName = param;
             that.mapChart();
           } else if (xmlhttp.status == 400) {
-            alert('There was an error 400');
+            alert("There was an error 400");
           } else {
-            alert('something else other than 200 was returned');
+            alert("something else other than 200 was returned");
           }
         }
       };
-      xmlhttp.open('GET', `/map/province/${eName}.json`, true);
+      xmlhttp.open("GET", `/map/province/${eName}.geoJson`, true);
       xmlhttp.send();
     },
 
@@ -271,16 +288,130 @@ export default {
             that.mapName = param;
             that.mapChart();
           } else if (xmlhttp.status == 400) {
-            alert('There was an error 400');
+            alert("There was an error 400");
           } else {
-            alert('something else other than 200 was returned');
+            alert("something else other than 200 was returned");
           }
         }
       };
 
-      xmlhttp.open('GET', `/map/city/${cName}.json`, true);
+      xmlhttp.open("GET", `/map/city/${cName}.geoJson`, true);
       xmlhttp.send();
     },
+
+    //读取地理信息
+    getMapInfo(fname, key, level = "province") {
+      // let xmlhttp = new XMLHttpRequest();
+      // let that = this;
+      // xmlhttp.onreadystatechange = function() {
+      //   if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+      //     if (xmlhttp.status == 200) {
+      //       const obj = JSON.parse(xmlhttp.responseText);
+      //       obj.name = name;
+      //       obj.key = key;
+      //       that.resData.push(obj);
+      //     } else if (xmlhttp.status == 400) {
+      //       alert("There was an error 400");
+      //     } else {
+      //       alert("something else other than 200 was returned");
+      //     }
+      //   }
+      // };
+      // xmlhttp.open("GET", `/map/province/${key}.geoJson`, true);
+      // xmlhttp.send();
+      // 引入GeoJSON库
+
+      // 加载GeoJSON文件
+      const url =
+        level === "city"
+          ? `/map/city/${key}.geoJson`
+          : `/map/province/${key}.geoJson`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          // 转换GeoJSON数据为Javascript对象
+          const featureCollection = turf.featureCollection(data.features);
+
+          // 创建一个空对象来存储省份数据
+          // const provinces = {};
+          if (level !== "city") {
+            this.resData[fname] = {
+              adcode: provinces[fname],
+              center: [],
+              children: {},
+            };
+            // 循环遍历每个Feature
+            featureCollection.features.forEach((feature) => {
+              // 获取省份名称
+              const name = feature.properties.name;
+              // 计算Feature的中心点坐标
+              const center = turf.center(feature).geometry.coordinates;
+              // 获取省份的行政代码（如果有）
+              const adcode = feature.properties.adcode.toString() || null;
+
+              // 将数据添加到provinces对象中
+              this.resData[fname].children[name] = {
+                center,
+                adcode,
+                children: {},
+              };
+
+              // 如果该Feature有子Feature，则将子Feature作为children添加到该省份对象中
+              if (feature.features) {
+                this.resData[fname].children[name].children = {};
+                feature.features.forEach((childFeature) => {
+                  const childName = childFeature.properties.name;
+                  const childCenter = turf.center(childFeature).geometry
+                    .coordinates;
+                  const childAdcode = childFeature.properties.adcode.toString() || null;
+                  this.resData[fname].children[name].children[childName] = {
+                    center: childCenter,
+                    adcode: childAdcode,
+                  };
+                });
+              }
+            });
+          } else {
+            // 循环遍历每个Feature
+            featureCollection.features.forEach((feature) => {
+              // 获取省份名称
+              const name = feature.properties.name;
+              // 计算Feature的中心点坐标
+              const center = turf.center(feature).geometry.coordinates;
+              // 获取省份的行政代码（如果有）
+              const adcode = feature.properties.adcode?.toString() || null;
+
+              // 将数据添加到provinces对象中
+              // this.resData[fname].children[name] = { center, adcode };
+
+              if (level === "city" && key === "710000") {
+                this.resData["台湾"].children[name] = {
+                  center,
+                  adcode,
+                };
+              } else {
+                Object.keys(this.resData).forEach((item) => {
+                  // console.log(item, name, center, adcode, fname);
+                  Object.keys(this.resData[item].children).forEach((it) => {
+                    if (it === fname) {
+                      this.resData[item].children[fname].children[name] = {
+                        center,
+                        adcode : adcode ? adcode.toString() : this.resData[item].children[fname].adcode.toString(),
+                      };
+                    }
+                  });
+                });
+              }
+            });
+          }
+          if (fname === "澳门") {
+            Object.keys(cityMap).forEach((item) => {
+              this.getMapInfo(item, cityMap[item], "city");
+            });
+          }
+        });
+    },
+    mapInfo() {},
   },
 };
 </script>
